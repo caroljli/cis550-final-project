e4e9e68612c4c82cd2d13a2349dac7e866880be6
DESKTOP-3ST4AD6 <mpriya946@gmail.com>
2021-05-08 23:41:11 -0400

    new review update

--- booknook/user/models.py
+++ booknook/user/models.py
@@ -15,6 +15,7 @@ class BookNookUser(models.Model):
         db_table = "BOOKNOOKUSER"
 
 class BookReview(models.Model):
+    ID = models.IntegerField()
     # review_id = models.IntegerField(primary_key=True, default=0)
     title = models.TextField()
     # author = models.ForeignKey(BookNookUser, on_delete=models.CASCADE)
