b0e444ccc2050a814efcde8a923bb49c00e9e97c
DESKTOP-3ST4AD6 <mpriya946@gmail.com>
2021-05-09 09:16:04 -0400

    added year bar

--- booknook/books/views.py
+++ booknook/books/views.py
@@ -4,6 +4,16 @@ from user.models import BookReview
 
 # logistical
 
+def splash(request):
+    # if request.user.is_authenticated:
+    #     if Student.objects.filter(user=request.user).count() > 0:
+    #         return redirect("/student_home")
+    #     else:
+    #         return redirect("/restaurant_home")
+    # else:
+	    # return render(request, "splash.html", {})
+    return render(request, "splash.html", {})
+
 def book_directory(request):
     logged_in = True
     search_params = request.GET.get("search_params")
@@ -14,6 +24,8 @@ def book_directory(request):
 
     genre_params = request.GET.get("genre_params")
     print(genre_params)
+
+    year_params = request.GET.get("year_params")
 
     # if authors_params:
     #     print("yes")
@@ -29,6 +41,12 @@ def book_directory(request):
     elif genre_params is not None:
         param_dict = { "param": genre_params }
         results = BestBook.objects.raw('SELECT DISTINCT * FROM BESTSELLERS b WHERE b.genre = %(param)s ORDER BY b.reviews DESC FETCH FIRST 10 rows only', param_dict)
+    # elif recommended_params is not None:
+    #     param_dict = { "param": recommended_params }
+    #     results = BestBook.objects.raw('WITH ratings AS (SELECT b.title, b.rating FROM BESTSELLERS b ORDER BY b.rating DESC), one_book AS (SELECT b.title, b.rating FROM BESTSELLERS b WHERE b.title = %(param)s) SELECT r.title FROM ratings r WHERE r.rating = rating FETCH FIRST 1 rows only', param_dict)
+    elif year_params is not None:
+        param_dict = { "param": year_params }
+        results = BestBook.objects.raw('SELECT DISTINCT * FROM BESTSELLERS b WHERE b.year >= %(param)s', param_dict)
     else: 
         results = Book.objects.all()
     return render(request, "book_directory.html", {"books": results, "logged_in": logged_in})
